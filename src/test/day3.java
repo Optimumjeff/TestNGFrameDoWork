3ed43ab8f372f565fe700355a51f05974e68e201
Jeffart Optimum <fifado6@gmail.com>
2018-10-19 22:48:37 +0200

    Prioriser les cas de test avec TestNG

---
+++ src/test/day3.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+	
+	@Test
+	public void WebLogincar()
+	{
+		//selenium
+		System.out.println("WebLogincar");
+	}
+	
+	@Test
+	public void MobileLogincar()
+	{
+		//Appium
+		System.out.println("Mobilelogincar");
+	}
+	
+	@Test
+	public void LoginAPIcar()
+	{
+		//Rest API
+		System.out.println("APILoginCar");
+	}
+
+}
---
+++ src/test/day4.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day4 {
+	
+	@Test
+	public void WebLoginHomeLoan()
+	{
+		//selenium
+		System.out.println("WebLoginHomeLoan");
+	}
+	
+	@Test
+	public void MobileLoginHomeLoan()
+	{
+		//Appium
+		System.out.println("MobileLoginHomeLoan");
+	}
+	
+	@Test
+	public void LoginAPIHomeLoan()
+	{
+		//Rest API
+		System.out.println("LoginAPIHomeLoan");
+	}
+
+}
